ba4191e5cf9863804ef452d71201c14848e22653
Martin Winkler <martin@werk85.de>
2023-10-08 13:05:22 +0200

    Added basic styling proposal

--- dist/plugin-wp-styles.php
+++ dist/plugin-wp-styles.php
@@ -4,8 +4,18 @@
  * Plugin URI:
  * Description:
  * Author:      Martin Winkler
- * Version:     1.0.2
+ * Version:     1.0.3
  */
-add_action('wp_enqueue_styles', function () {
-	wp_enqueue_style('plugin-wp-styles', plugins_url('index.css', __FILE__));
+add_action( 'wp_enqueue_scripts', 'addStyles' );
+add_action( 'admin_enqueue_scripts', 'addStyles' );
+
+function addStyles() {
+	$path = plugin_dir_path( __FILE__ );
+	$url = plugins_url( 'styles.css', __FILE__ );
+	wp_enqueue_style( 'plugin-wp-styles', $url, array(), filemtime( $path ) );
+}
+
+add_action('body_class', function ($classes) {
+  $classes[] = 'plugin-wsc';
+  return $classes;
 });
