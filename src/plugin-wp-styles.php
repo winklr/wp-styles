6209220081aa7e220245e1723270e60f1acce992
Martin Winkler <martin@werk85.de>
2023-10-08 10:50:06 +0200

    Version bump to 1.0.1

--- src/plugin-wp-styles.php
+++ src/plugin-wp-styles.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description:
  * Author:      Martin Winkler
- * Version:     1.0.0
+ * Version:     1.0.1
  */
 add_action('wp_enqueue_styles', function () {
 	wp_enqueue_style('plugin-wp-styles', plugins_url('index.css', __FILE__));
